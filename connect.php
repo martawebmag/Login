87ca313170401f1a14ebe21bdc9388f15e555790
Marta Gałecka <galecka_marta@wp.pl>
2024-05-22 11:30:27 +0200

    add

--- connect.php
+++ connect.php
@@ -3,11 +3,6 @@
 $host = "localhost";
 $db_user = "root";
 $db_password = "";
-<<<<<<< HEAD
-$db_name = "fundacja";
-=======
 $db_name = "uzytkownicy";
->>>>>>> df83254a97380b5030ef9b98e95a709c3c37b857
-
 
 ?>
